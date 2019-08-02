f662c8859fe66af5bf49eb66b99a9eb27a2fe782
remote.react <remote.react@gmail.com>
2019-08-02 10:01:12 +0430

    adding project desctiption

--- src/layout/Section.js
+++ src/layout/Section.js
@@ -6,8 +6,9 @@ const Section = () =>{
     return(
         <div id='section'>
             <div className='item1'>
-            <h1>This is my portfolio inspired by codepen.com </h1>
-            <p>You can email me at: &nbsp;
+            <h1>my portfolio
+            if you're interested</h1>
+            <p>Please email me at: &nbsp;
                 <strong>remote.react@gmail.com</strong>
             </p>
                 <a href="#">hard work pays off</a>
--- src/layout/Vh.js
+++ src/layout/Vh.js
@@ -30,8 +30,8 @@ class vh extends Component {
                                             <a  href="https://github.com/remotereact/youtube">repo</a>
                                             <a href="https://serene-ramanujan-c85fbd.netlify.com/">link</a>
                                         </div>
-                                        <p className='project-description'>Lorem ipsum dolor sit amet.C7C9D3
-                                            Lorem ipsum dolor sit amet.C7C9D3Lorem ipsum dolor sit amet.C7C9D3</p>
+                                        <p className='project-description'>Youtube project I have used youtube api in order
+                                        to get the data (json file) from youtube, this project is created by react</p>
                                     </div>
 
                                 </div>
@@ -57,8 +57,8 @@ class vh extends Component {
                                             <a  href="https://github.com/remotereact/animals">repo</a>
                                             <a href="https://brave-bhabha-a65268.netlify.com/">link</a>
                                         </div>
-                                        <p className='project-description'>Lorem ipsum dolor sit amet.C7C9D3
-                                            Lorem ipsum dolor sit amet.C7C9D3Lorem ipsum dolor sit amet.C7C9D3</p>
+                                        <p className='project-description'>this project created by react and redux, redux is used for state management,
+                                       and get request with axios for getting some animals data from an animal api</p>
                                     </div>
                                 </div>
                             )}
@@ -79,9 +79,9 @@ class vh extends Component {
                                             <a href="https://github.com/remotereact/auto-grid-images">repo</a>
                                             <a href="https://determined-gates-a6f62b.netlify.com/">link</a>
                                         </div>
-                                        <p className='project-description'>Lorem ipsum dolor sit amet.C7C9D
-                                            Lorem ipsum dolor sit amet.C7C9D3
-                                            Lorem ipsum dolor sit amet.C7C9D33</p>
+                                        <p className='project-description'>this one is quite interesting because when we get the
+                                        data from unsplash api which is a picture api , it will gives us pictures with different sizes,
+                                        but this app will resize them automatically</p>
                                     </div>
                                 </div>
                             )}
@@ -107,9 +107,7 @@ class vh extends Component {
                                             <a href="">link</a>
                                         </div>
 
-                                        <p className='project-description'>Lorem ipsum dolor sit amet.C7C9D3
-                                            Lorem ipsum dolor sit amet.C7C9D3
-                                            Lorem ipsum dolor sit amet.C7C9D3</p>
+                                        <p className='project-description'>I'm currently working on this project </p>
 
                                     </div>
                                 </div>
@@ -135,9 +133,7 @@ class vh extends Component {
                                             <a href="">repo</a>
                                             <a href="">link</a>
                                         </div>
-                                        <p className='project-description'>Lorem ipsum dolor sit amet.C7C9D3
-                                            Lorem ipsum dolor sit amet.C7C9D3
-                                            Lorem ipsum dolor sit amet.C7C9D3</p>
+                                        <p className='project-description'>I'm currently working on this project</p>
                                     </div>
                                 </div>
                             )}
